d8401a7e42798df5c0190c415a9eca65500079ab
Barath Vedantham <vedant.barath@gmail.com>
2025-07-08 13:08:34 -0700

    added basic logging middleware

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import (
 	"net/http"
 )
 
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	dir := flag.String("dir", ".", "directory to serve files from")
 	port := flag.Int("port", 8080, "port to listen on")
@@ -15,7 +22,7 @@ func main() {
 
 	fs := http.FileServer(http.Dir(*dir))
 
-	http.Handle("/", fs)
+	http.Handle("/", loggingMiddleware(fs))
 
 	addr := fmt.Sprintf(":%d", *port)
 	log.Printf("Serving %s on port %d", *dir, *port)
